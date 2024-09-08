9713b28c6a7d9d92a0c2bc8921cd5865ad53142c
hashf0rd <124741545+hashf0rd@users.noreply.github.com>
2024-09-08 12:41:11 +0100

    added attack path comments

--- Lab-Configuration/ACLAbuse.ps1
+++ Lab-Configuration/ACLAbuse.ps1
@@ -21,12 +21,13 @@ foreach ($group in  @('Office Admins','IT Admins','DevOps')) {
     New-ADGroup -Name $group -GroupScope Global -Path 'OU=EnterpriseUsers,DC=aclabuse,DC=lab'
 }
 
-Add-ADGroupMember -Identity 'Office Admins' -Members 'jane.doe'
+Add-ADGroupMember -Identity 'IT Admins' -Members 'jane.doe'
+Add-ADGroupMember -Identity 'Office Admins' -Members 'john.dee'
 
 New-ADComputer -Name "SRV2-000" -SamAccountName "SRV2-000" -Path "OU=ApplicationServers,DC=aclabuse,DC=lab"
 
 # WriteProperty
-$targetDN =  "AD:"+(Get-ADGroup -Identity 'Domain Users').DistinguishedName
+$targetDN =  "AD:$(Get-ADGroup -Identity 'Office Admins')"
 $targetACL = Get-ACL -Path $targetDN
 $groupSID = (Get-ADGroup -Identity 'Office Admins').SID
 $ace = New-Object System.DirectoryServices.ActiveDirectoryAccessRule(
@@ -34,12 +35,44 @@ $ace = New-Object System.DirectoryServices.ActiveDirectoryAccessRule(
     [System.DirectoryServices.ActiveDirectoryRights]::WriteProperty,
     [System.Security.AccessControl.AccessControlType]::Allow
     )
-$targetACL.SetAccessRuleProtection($true, $true)
 $targetACL.SetAccessRule($ace)
-Set-Acl -AclObject $objectACL -Path $targetDN
+Set-Acl -AclObject $targetACL -Path $targetDN
 
-# Self
-# WriteOwner
-# WriteDACL
-# GenericWrite
-# GenericAll
+<#
+Attack Path
+
+Reconnaissance:
+The attacker gains initial access to the network and discovers John's account has a weak password.
+
+Initial Compromise (WriteProperty):
+
+The attacker compromises John's account.
+
+Jane has WriteProperty rights on John's account for the 'scriptPath' attribute.
+Attacker modifies John's scriptPath to include a malicious script that will run on John's next login.
+
+
+Lateral Movement (Self):
+
+John logs in, executing the malicious script.
+The script exploits John's "Self" permissions on his account to add his account to the "Office Admins" group.
+
+
+Privilege Escalation (WriteDACL):
+
+As a member of "Office Admins", John has WriteDACL permissions on the "IT Admins" group.
+The attacker, through John's account, modifies the DACL of "IT Admins" to grant John's account GenericWrite permissions.
+
+
+Further Compromise (GenericWrite):
+
+Using the newly acquired GenericWrite permissions on "IT Admins", the attacker adds John to this group.
+As an "IT Admin", John now has elevated privileges across various systems.
+
+
+Domain Dominance (GenericAll):
+
+The "IT Admins" group has GenericAll permissions on the "DevOps Admins" group.
+The attacker, through John's "IT Admin" privileges, grants John's account direct membership in "DevOps Admins".
+"DevOps Admins" has extensive privileges, including potential access to critical infrastructure and deployment pipelines.
+#>
